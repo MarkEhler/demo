6b30af803654a3a61443a4837a327f3e8d73ecca
Mark Ehler <ehlerm42@gmail.com>
2026-06-04 09:40:35 -0600

    rbac

--- application/deploy/dd-powershell-agent.ps1
+++
@@ -1,5 +0,0 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-$env:DD_API_KEY = '6f5d0ed636e7f1129d4c6aa388f00bff';
-$env:DD_SITE = 'us5.datadoghq.com';
-(New-Object System.Net.WebClient).DownloadFile('https://install.datadoghq.com/datadog-installer-x86_64.exe', 'C:\Windows\SystemTemp\datadog-installer-x86_64.exe');
-C:\Windows\SystemTemp\datadog-installer-x86_64.exe
