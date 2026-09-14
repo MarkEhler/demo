94155c632ffbb3b883a531a41b58b7144de64dd2
Mark Ehler <ehlerm42@gmail.com>
2026-09-14 17:04:14 -0600

    session token placeholder

--- application/terraform/variables.tf
+++ application/terraform/variables.tf
@@ -28,15 +28,9 @@ variable "aws_iam_user_arn" {
   default     = ""
 }
 
-variable "aws_access_key_id" {
+variable "aws_session_token" {
   type        = string
-  description = "AWS access key used for Terraform authentication. For production, prefer OIDC or IAM role assumptions."
-  default     = ""
-}
-
-variable "aws_secret_access_key" {
-  type        = string
-  description = "AWS secret access key used for Terraform authentication."
+  description = "Optional AWS session token for temporary credentials. Leave blank if not required."
   default     = ""
 }
 
